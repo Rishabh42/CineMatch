12079784aaf1e289f3034b243039b59dfed68532
Varun <varunshiri99@gmail.com>
2023-11-21 14:15:38 -0500

    Fix bugs in app.py movie_rec.py and update requirements.txt

--- app/app.py
+++ app/app.py
@@ -29,13 +29,14 @@ def get_user_details(userid):
     try:
         response = requests.get(
             "http://fall2023-comp585.cs.mcgill.ca:8080/user/"+str(userid))
+
+        if response.status_code != 200:
+            return "Response not successful"
+
+        return response.json()
     except Exception as exc:
         print(f"Unexpected exception raised while getting user details: {exc}")
-
-    if response.status_code != 200:
-        return "Response not successful"
-
-    return response.json()
+        return "Error: " + str(exc)
 
 
 def predict_movies(userid):
@@ -48,11 +49,11 @@ def predict_movies(userid):
     # these may be needed to send to the model.
     try:
         prediction = get_recommendation(userid)
+        return ",".join(prediction)
     except Exception as exc:
         print(
             f"Unexpected exception raised while getting movie predictions: {exc}")
-
-    return ",".join(prediction)
+        return "Error: " + str(exc)
 
 
 @app.route("/")
