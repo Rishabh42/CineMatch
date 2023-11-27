8cd75931bc28c7cd06ceb892294f129917dd7d35
Yaoqiang WU <yaoqiang.wu@mail.mcgill.ca>
2023-11-27 04:45:41 +0000

    Fixed bug

--- app/data_processing_scripts/kafka_consumer_data_appender.py
+++ app/data_processing_scripts/kafka_consumer_data_appender.py
@@ -2,14 +2,16 @@ import subprocess
 import time
 import os
 
-def run_kafka_consumer(duration_in_minutes):
+DATA_COLLECTION_PATH = 'dummy_data_test/ratings.csv'
+CLEANED_DATA_PATH = 'cleaned_rating.csv'
+TARGET_FILE_PATH = '/app/data/cleaned_rating.csv'
 
+def run_kafka_consumer(duration_in_minutes):
     kafka_command = ("docker run -it --log-opt max-size=50m --log-opt max-file=5 "
                      "bitnami/kafka kafka-console-consumer.sh "
                      "--bootstrap-server fall2023-comp585.cs.mcgill.ca:9092 "
                      "--topic movielog4")
-
-    # Run the command and collect data for the specified duration
+    
     start_time = time.time()
     process = subprocess.Popen(kafka_command, shell=True, stdout=subprocess.PIPE)
 
@@ -19,27 +21,23 @@ def run_kafka_consumer(duration_in_minutes):
             break
 
     output, _ = process.communicate()
-    with open('kafka_output.txt', 'wb') as file:
+    with open(DATA_COLLECTION_PATH, 'wb') as file:
         file.write(output)
 
-# def run_processing_script():
-#     os.system('/app/data_processing_scripts/process_rating.py kafka_output.txt')
+def run_processing_script():
+    os.system('/app/data_processing_scripts/process_rating.py')
 
-# def append_to_cleaned_data():
-#     with open('/app/data/cleaned_rating.csv', 'a') as outfile, open('cleaned_rating.csv', 'r') as infile:
-#         outfile.write(infile.read())
+def append_to_cleaned_data():
+    with open(TARGET_FILE_PATH, 'a') as outfile, open(CLEANED_DATA_PATH, 'r') as infile:
+        next(infile)  # Skip the header of the new file
+        outfile.write(infile.read())
 
 def main():
-    # Run Kafka Consumer for 15 minutes
     run_kafka_consumer(15)
-
-    time.sleep(300)
-
+    time.sleep(300)  
     run_processing_script()
-
     append_to_cleaned_data()
-
-    os.remove('cleaned_rating.csv')
+    os.remove(CLEANED_DATA_PATH)
 
 if __name__ == "__main__":
     main()
