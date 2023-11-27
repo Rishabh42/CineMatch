9053e84abb573839920204d3e58879f49a0f40d8
Yaoqiang WU <yaoqiang.wu@mail.mcgill.ca>
2023-11-27 05:27:49 +0000

    Fixed bugs

--- app/data_processing_scripts/kafka_consumer_data_appender.py
+++ app/data_processing_scripts/kafka_consumer_data_appender.py
@@ -7,37 +7,40 @@ CLEANED_DATA_PATH = 'cleaned_rating.csv'
 TARGET_FILE_PATH = '/app/data/cleaned_rating.csv'
 
 def run_kafka_consumer(duration_in_minutes):
-    kafka_command = ("docker run -it --log-opt max-size=50m --log-opt max-file=5 "
+    kafka_command = ("docker run --log-opt max-size=50m --log-opt max-file=5 "
                      "bitnami/kafka kafka-console-consumer.sh "
                      "--bootstrap-server fall2023-comp585.cs.mcgill.ca:9092 "
                      "--topic movielog4")
-    
+
+    process = subprocess.Popen(kafka_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
     start_time = time.time()
-    process = subprocess.Popen(kafka_command, shell=True, stdout=subprocess.PIPE)
+    output = []
 
-    while True:
-        if time.time() - start_time > duration_in_minutes * 60:
-            process.terminate()
-            break
+    try:
+        while time.time() - start_time <= duration_in_minutes * 60:
+            line = process.stdout.readline()
+            if line:
+                output.append(line.decode('utf-8').strip())
+    finally:
+        process.kill()
 
-    output, _ = process.communicate()
-    with open(DATA_COLLECTION_PATH, 'wb') as file:
-        file.write(output)
+    with open(DATA_COLLECTION_PATH, 'w') as file:
+        for line in output:
+            file.write(line + '\n')
 
 def run_processing_script():
     os.system('/app/data_processing_scripts/process_rating.py')
 
 def append_to_cleaned_data():
     with open(TARGET_FILE_PATH, 'a') as outfile, open(CLEANED_DATA_PATH, 'r') as infile:
-        next(infile)  # Skip the header of the new file
+        next(infile)  
         outfile.write(infile.read())
 
 def main():
     run_kafka_consumer(15)
-    time.sleep(300)  
     run_processing_script()
     append_to_cleaned_data()
-    os.remove(CLEANED_DATA_PATH)
 
 if __name__ == "__main__":
     main()
