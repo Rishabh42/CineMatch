f00f38db1c036b1d235eb7b8401f4d8bf5bfe749
rthane <rishabh.thaney@mail.mcgill.ca>
2023-11-26 22:22:42 -0500

    git commit logic & TODOs

--- app/auto_deployment/auto_deploy.py
+++ app/auto_deployment/auto_deploy.py
@@ -1,17 +1,34 @@
 import os
 import sys
+import subprocess
+
+# Update the repo
+subprocess.run(["git", "pull"])
 
 sys.path.append("../")
 from model.movie_rec import train
 from model.movie_rec import test_collaborative_filtering
 
-# os.chdir("../")
 CURR = os.getcwd()
 train(data_path=os.path.join(CURR, 'data'))
-rmse_score = test_collaborative_filtering()
+# rmse_score = test_collaborative_filtering()
+rmse_score = 1.2
+
+# TODO: Data collection script
 
 if rmse_score < 1:
+    # TODO: Success mail
     with open("auto_deployment/version.txt", "r") as f:
         version = f.read()
     next_version = str(int(version) + 1)
-    print(next_version)
+
+    # Write the updated version back to the file
+    with open("auto_deployment/version.txt", "w") as f:
+        f.write(next_version)
+
+    subprocess.run(["git", "add", "auto_deployment/version.txt"])
+    subprocess.run(["git", "commit", "-m", "model version update" + next_version])
+    subprocess.run(["git", "push"])
+else:
+    # unsuccessful mail
+    print(f"Deployment of new model failed since RMSE score of {rmse_score} is too high !")
