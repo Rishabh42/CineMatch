fc883bdfa8439230780e446ec19a88997065598c
rthane <rishabh.thaney@mail.mcgill.ca>
2023-11-28 00:41:16 -0500

    model test fix

--- app/model/movie_rec.py
+++ app/model/movie_rec.py
@@ -10,8 +10,8 @@ import os
 os.chdir("../")
 CURR = os.getcwd()
 
-MODEL_PATH = os.path.join(CURR, 'app', 'model', 'model.pkl')
-DATA_PATH = os.path.join(CURR, 'app', 'data')
+MODEL_PATH = os.path.join(CURR, '..', 'model', 'model.pkl')
+DATA_PATH = os.path.join(CURR, '..', 'data')
 
 FILE_NAME_USERS = 'user_data.csv'
 FILE_NAME_RATINGS = 'cleaned_rating.csv'
