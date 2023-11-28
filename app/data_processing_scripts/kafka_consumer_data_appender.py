ecbc4098be32718effc3d9b923460eea6ac18da1
Yaoqiang WU <yaoqiang.wu@mail.mcgill.ca>
2023-11-28 20:48:23 +0000

    fixed base dir

--- app/data_processing_scripts/kafka_consumer_data_appender.py
+++ app/data_processing_scripts/kafka_consumer_data_appender.py
@@ -1,12 +1,13 @@
-import subprocess
 import os
+import subprocess
 import time
 
-BASE_DIR = os.path.dirname(__file__)
+# This should be the root directory of your app, adjust accordingly.
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DATA_COLLECTION_DIR = os.path.join(BASE_DIR, 'dummy_data_test')
 DATA_COLLECTION_PATH = os.path.join(DATA_COLLECTION_DIR, 'ratings.csv')
 CLEANED_DATA_PATH = os.path.join(BASE_DIR, 'cleaned_rating.csv')
-TARGET_FILE_PATH = os.path.join('/app/data', 'cleaned_rating.csv')
+TARGET_FILE_PATH = os.path.join(BASE_DIR, 'data', 'cleaned_rating.csv')
 
 def ensure_directory_exists(directory):
     if not os.path.exists(directory):
@@ -15,11 +16,13 @@ def ensure_directory_exists(directory):
 def run_kafka_consumer(duration_in_minutes):
     ensure_directory_exists(DATA_COLLECTION_DIR)
 
-    kafka_command = ("docker run --log-opt max-size=50m --log-opt max-file=5 "
-                     "bitnami/kafka kafka-console-consumer.sh "
-                     "--bootstrap-server fall2023-comp585.cs.mcgill.ca:9092 "
-                     "--topic movielog4")
-
+    kafka_command = (
+        "docker run --log-opt max-size=50m --log-opt max-file=5 "
+        "bitnami/kafka kafka-console-consumer.sh "
+        "--bootstrap-server fall2023-comp585.cs.mcgill.ca:9092 "
+        "--topic movielog4"
+    )
+    
     process = subprocess.Popen(kafka_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     start_time = time.time()
@@ -37,20 +40,20 @@ def run_kafka_consumer(duration_in_minutes):
         file.writelines(line + '\n' for line in output)
 
 def run_processing_script():
-    os.system(f'python /app/data_processing_scripts/process_rating.py {DATA_COLLECTION_PATH}')
+    processing_command = f'python3 {os.path.join(BASE_DIR, "data_processing_scripts", "process_rating.py")} {DATA_COLLECTION_PATH}'
+    os.system(processing_command)
 
 def append_to_cleaned_data():
-    with open(CLEANED_DATA_PATH, 'r') as infile:
-        lines = infile.readlines()
+    if not os.path.isfile(CLEANED_DATA_PATH):
+        print(f"Processed file {CLEANED_DATA_PATH} does not exist.")
+        return
 
-    with open(TARGET_FILE_PATH, 'a') as outfile:
-        if os.path.getsize(TARGET_FILE_PATH) > 0:  #
-            lines = lines[1:]  
-        outfile.writelines(lines)
+    with open(CLEANED_DATA_PATH, 'r') as infile, open(TARGET_FILE_PATH, 'a') as outfile:
+        next(infile)  # Skip the header
+        outfile.writelines(infile.readlines())
 
 def main():
     run_kafka_consumer(1)
-    time.sleep(300)  # Optional: sleep for a bit
     run_processing_script()
     append_to_cleaned_data()
     os.remove(CLEANED_DATA_PATH)
