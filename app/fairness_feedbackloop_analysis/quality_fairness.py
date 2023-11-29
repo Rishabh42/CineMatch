2915121c2d6ded2c0b29b6c142f568f09f498875
tamaraaPrs <100276092+tamaraaPrs@users.noreply.github.com>
2023-11-29 16:22:06 -0500

    rename file

--- app/fairness_feedbackloop_analysis/quality_fairness.py
+++ app/fairness_feedbackloop_analysis/quality_fairness.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 from matplotlib import pyplot as plt
-from app.model.movie_rec import test_collaborative_filtering_subset, train
+from app.model.movie_rec import test_collaborative_filtering_subset, train, load_dataset_from_csv
 
 os.chdir("../")
 CURR = os.getcwd()
@@ -14,12 +14,21 @@ FILE_NAME_RATINGS = 'cleaned_rating.csv'
 FILE_NAME_MOVIES = 'filtered_responses.csv'
 
 
+def get_labels(column_name):
+    data = load_dataset_from_csv(DATA_PATH, FILE_NAME_USERS, ['user_id', 'age', 'occupation', 'gender'])
+    return data[column_name].unique().tolist()
+
+
 def gender_rmse():
-    return test_collaborative_filtering_subset('gender', ['M', 'F'])
+    return test_collaborative_filtering_subset('gender', get_labels('gender'))
 
 
 def age_rmse():
-    return test_collaborative_filtering_subset('age', range(0, 100))
+    return test_collaborative_filtering_subset('age', get_labels('age'))
+
+
+def occupation_rmse():
+    return test_collaborative_filtering_subset('occupation', get_labels('occupation'))
 
 
 if __name__ == '__main__':
@@ -27,3 +36,4 @@ if __name__ == '__main__':
           file_name_ratings=FILE_NAME_RATINGS, model_path=MODEL_PATH)
     print(gender_rmse())
     print(age_rmse())
+    print(occupation_rmse())
