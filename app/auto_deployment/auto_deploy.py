b378a42a3d26b73a6627c8f04de561fc27ff7b65
akapur12 <aayush.kapur@mail.mcgill.ca>
2023-11-29 03:34:34 -0500

    Provenance dvc versioning #44 #45 #61

--- app/auto_deployment/auto_deploy.py
+++ app/auto_deployment/auto_deploy.py
@@ -2,6 +2,7 @@ import os
 import sys
 import subprocess
 from emailer import send_email
+from data_versioning import model_data_versioning
 
 # Update the repo
 subprocess.run(["git", "pull"])
@@ -14,6 +15,7 @@ from data_processing_scripts.kafka_consumer_data_appender import run_kafka_consu
 
 CURR = os.getcwd()
 # print("---- CURR ----:",CURR)
+# the current directory is the app directory
 
 # Data collection & pre-processing
 # TODO: use 15 minutes
@@ -34,13 +36,22 @@ if rmse_score < 1:
     with open("auto_deployment/version.txt", "w") as f:
         f.write(next_version)
 
+    subprocess.run(["dvc", "add", "model/model.pkl"])
+
+    # Data versioning which can be offloaded to remote storage
+    model_data_versioning(next_version,"./data", "./model")
+
+    subprocess.run(["git", "add", "model/model.pkl.dvc", "model/.gitignore"])
+    subprocess.run(["git", "add", "data/versioning", "model/versioning"])
     subprocess.run(["git", "add", "auto_deployment/version.txt"])
     subprocess.run(["git", "add", "data/cleaned_rating.csv"])
-    subprocess.run(["git", "commit", "-m", "model version update" + next_version])
+    subprocess.run(["git", "commit", "-m", "Data and Model version update: [" + next_version + "]"])
     subprocess.run(["git", "push"])
 
     # Success mail
     send_email("Successfully deployed new model","Your deployment of the new model was successful !")
+
+
 else:
     # unsuccessful mail
     send_email(f"Deployment of new model failed","Deployment of new model failed since RMSE score of {rmse_score} is too high !")
