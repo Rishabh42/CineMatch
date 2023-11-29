f35ae785291f77bb130b96ee6e573b5dbdc86342
Aayush Kapur <aayush.kapur@mail.mcgill.ca>
2023-11-29 18:18:37 +0000

    Data-integration -> development

--- app/auto_deployment/auto_deploy.py
+++ app/auto_deployment/auto_deploy.py
@@ -2,6 +2,7 @@ import os
 import sys
 import subprocess
 from emailer import send_email
+from data_versioning import model_data_versioning
 
 # Update the repo
 subprocess.run(["git", "pull"])
@@ -10,11 +11,19 @@ sys.path.append("../")
 # os.chdir('../')
 from model.movie_rec import train
 from model.movie_rec import test_collaborative_filtering
+from data_processing_scripts.kafka_consumer_data_appender import run_kafka_consumer, run_processing_script, append_to_cleaned_data, cleanup_containers
 
 CURR = os.getcwd()
 # print("---- CURR ----:",CURR)
+# the current directory is the app directory
 
-# TODO: Data collection script
+# Data collection & pre-processing
+# TODO: use 15 minutes
+run_kafka_consumer(1)
+run_processing_script()
+append_to_cleaned_data()
+cleanup_containers()
+
 # train the model with new data
 train(data_path=os.path.join(CURR, 'data'))
 rmse_score = test_collaborative_filtering()
@@ -28,13 +37,22 @@ if rmse_score < 1:
     with open("auto_deployment/version.txt", "w") as f:
         f.write(next_version)
 
-    # TODO: Push the new cleaned_rating.csv instead of version.txt
+    subprocess.run(["dvc", "add", "model/model.pkl"])
+
+    # Data versioning which can be offloaded to remote storage
+    model_data_versioning(next_version,"./data", "./model")
+
+    subprocess.run(["git", "add", "model/model.pkl.dvc", "model/.gitignore"])
+    subprocess.run(["git", "add", "data/versioning", "model/versioning"])
     subprocess.run(["git", "add", "auto_deployment/version.txt"])
-    subprocess.run(["git", "commit", "-m", "model version update" + next_version])
+    subprocess.run(["git", "add", "data/cleaned_rating.csv"])
+    subprocess.run(["git", "commit", "-m", "Data and Model version update: [" + next_version + "]"])
     subprocess.run(["git", "push"])
 
     # Success mail
     send_email("Successfully deployed new model","Your deployment of the new model was successful !")
+
+
 else:
     # unsuccessful mail
     send_email(f"Deployment of new model failed","Deployment of new model failed since RMSE score of {rmse_score} is too high !")
