cb2a08f97050a6e1f9d15b62473c4ec8914af4e1
rthane <rishabh.thaney@mail.mcgill.ca>
2023-11-28 19:10:29 -0500

    email notifications

--- app/auto_deployment/auto_deploy.py
+++ app/auto_deployment/auto_deploy.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+from emailer import send_email
 
 # Update the repo
 subprocess.run(["git", "pull"])
@@ -11,14 +12,14 @@ from model.movie_rec import train
 from model.movie_rec import test_collaborative_filtering
 
 CURR = os.getcwd()
-print("---- CURR ----:",CURR)
+# print("---- CURR ----:",CURR)
 
 # TODO: Data collection script
+# train the model with new data
 train(data_path=os.path.join(CURR, 'data'))
 rmse_score = test_collaborative_filtering()
 
 if rmse_score < 1:
-    # TODO: Success mail
     with open("auto_deployment/version.txt", "r") as f:
         version = f.read()
     next_version = str(int(version) + 1)
@@ -30,6 +31,10 @@ if rmse_score < 1:
     subprocess.run(["git", "add", "auto_deployment/version.txt"])
     subprocess.run(["git", "commit", "-m", "model version update" + next_version])
     subprocess.run(["git", "push"])
+
+    # Success mail
+    send_email("Successfully deployed new model","Your deployment of the new model was successful !")
 else:
     # unsuccessful mail
+    send_email(f"Deployment of new model failed","Deployment of new model failed since RMSE score of {rmse_score} is too high !")
     print(f"Deployment of new model failed since RMSE score of {rmse_score} is too high !")
